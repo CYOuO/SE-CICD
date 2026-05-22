434aae4e8bb956a8f36a2e7ef38066b62dbc6ad5
cy_ouo <joy1001234@gmail.com>
2026-05-22 11:44:14 +0800

    calculator API

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,8 @@
-def add_func(a, b):
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def add_func(a: float, b: float) -> float:
     return a + b
 
 
@@ -10,3 +14,12 @@ def sub_func(a, b):
 def mul_func(a, b):
     # return "123"
     return a * b
+
+@app.get("/")
+def home():
+    return {"status": "Online","message": "這是簡易計算機 API"}
+
+@app.get("/add")
+def calculate_add(a: float, b: float):
+    result = add_func(a, b)
+    return {"operation": "addition", "a":a, "b":b, "result": result}
